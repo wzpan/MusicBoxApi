cf8cfc7a14b6b6d35c50ede58d5a8ceeef0bcf09
Joseph Pan <m@hahack.com>
2017-05-18 23:58:31 +0800

    发布 1.0.2

--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ with open(path.join(here, 'README.rst'), encoding='utf-8') as f:
 setup(
     name='MusicBoxApi',
 
-    version='1.0.0',
+    version='1.0.2',
 
     description='NetEase Music Api',
     long_description=long_description,
